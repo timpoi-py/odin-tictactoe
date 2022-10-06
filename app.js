d3d3f0ab7e753ca622df7ad4d354cbbf8bcb6a97
Timothy Paul Ignacio <timothypaulignacio@gmail.com>
2022-10-06 16:11:58 +0800

    add logic

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const tiles = document.querySelectorAll("[data-tile]");
+const startBtn = document.getElementById("start-btn");
+const restartBtn = document.getElementById("restart-btn");
+const gameOverModal = document.querySelector(".game-over");
+const gameOverBg = document.querySelector(".game-over-bg");
+const announceWinner = document.querySelector(".announce");
+const playAgainBtn = document.querySelector(
+  ".game-over input[value='Play Again']"
+);
+const turnParag = document.querySelector(".game p");
+
 const WIN_COMBINATION = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,27 +22,103 @@ const WIN_COMBINATION = [
 
 let xTurn = true;
 
-tiles.forEach((tile) => {
-  tile.addEventListener(
-    "click",
-    (e) => {
-      gameLogic(e);
-    },
-    {
-      once: true,
-    }
-  );
-});
+startBtn.onclick = () => {
+  removeStartBtn();
+  displayHover();
+  startGame();
+};
+
+playAgainBtn.onclick = () => {
+  hideGameOverModal();
+  removeXOClasses();
+  xTurn = true;
+  displayHover();
+  startGame();
+};
+
+function hideGameOverModal() {
+  gameOverBg.style.display = "none";
+  gameOverModal.style.scale = "0";
+}
+
+function removeXOClasses() {
+  tiles.forEach((tile) => {
+    tile.classList.remove("x");
+    tile.classList.remove("o");
+  });
+}
+
+function removeStartBtn() {
+  startBtn.style.visibility = "hidden";
+}
+
+function showRestartBtn() {
+  restartBtn.style.display = "inline";
+}
+
+function startGame() {
+  tellTurns();
+  tiles.forEach((tile) => {
+    tile.addEventListener(
+      "click",
+      (e) => {
+        gameLogic(e);
+      },
+      {
+        once: true,
+      }
+    );
+  });
+}
 
 function gameLogic(e) {
   placeMarker(e);
+
   if (checkWinner()) {
     if (xTurn) {
+      announceWinner.textContent = "X Wins!";
       console.log(" X WINS");
-    } else console.log(" O WINS");
+      showGameOverModal();
+    } else if (!xTurn) {
+      announceWinner.textContent = "O Wins!";
+      console.log(" O WINS");
+      showGameOverModal();
+    }
+  } else if (checkDraw()) {
+    announceWinner.textContent = "DRAW!";
+    console.log("DRAW");
+    showGameOverModal();
   }
+
   switchTurn();
+  tellTurns();
+
   displayHover();
+}
+
+function tellTurns() {
+  if (xTurn) {
+    turnParag.textContent = "X Turn";
+  } else if (!xTurn) {
+    turnParag.textContent = "O Turn";
+  }
+}
+
+function checkDraw() {
+  return [...tiles].every((tile) => {
+    if (tile.classList.contains("x") | tile.classList.contains("o")) {
+      return true;
+    }
+  });
+}
+
+window.onclick = () => {
+  console.log(checkDraw());
+};
+
+function showGameOverModal() {
+  gameOverBg.style.display = "flex";
+  gameOverModal.style.scale = "1";
 }
 
 function checkWinner() {
@@ -52,14 +138,14 @@ function checkWinner() {
 }
 
 function displayHover() {
-  deleteHoverDisplay();
+  removeHoverDisplay();
   tiles.forEach((tile) => {
     if (xTurn) tile.classList.add("tile-x");
     else if (!xTurn) tile.classList.add("tile-o");
   });
 }
 
-function deleteHoverDisplay() {
+function removeHoverDisplay() {
   tiles.forEach((tile) => {
     tile.classList.remove("tile-x");
     tile.classList.remove("tile-o");
